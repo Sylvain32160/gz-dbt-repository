f46e790bd60513bef63038edfe9e0a0e055d27f1
sylvain.delon33800@gmail.com <sylvain.delon33800@gmail.com>
2026-02-04 14:51:22 +0000

    End of exercise #3

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,22 @@
+-- stg_raw_ship.sql
+
+ with
+
+ source as (
+
+     select * from {{ source('raw', 'ship') }}
+
+ ),
+
+ renamed as (
+
+     select
+          orders_id
+         ,shipping_fee
+         ,logCost AS log_cost
+         ,SAFE_CAST(ship_cost AS FLOAT64) AS ship_cost
+     from source
+
+ )
+
+ select * from renamed
