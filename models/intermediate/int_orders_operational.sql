cd5137cae73df41649f91ace5c4b34a9b7310bd7
Sylvain DELON <sylvain.delon33800@gmail.com>
2026-02-05 14:10:46 +0000

    Création du mart Finance_campaigns_day

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -7,6 +7,7 @@ SELECT
   ,shipping_fee
   ,purchase_cost
   ,margin
+  ,ship_cost
   ,log_cost
   ,log_cost + ship_cost AS operational_cost 
   ,ROUND((margin + shipping_fee - log_cost - ship_cost),2) AS operational_margin
--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -7,6 +7,7 @@ SELECT
     ,ROUND(AVG(revenue),2) AS average_basket
     ,ROUND(SUM(operational_margin),2) AS operational_margin
     ,ROUND(SUM(purchase_cost),2) AS purchase_cost
+    ,ROUND(SUM(ship_cost),2) AS ship_cost
     ,ROUND(SUM(shipping_fee),2) AS shipping_fee
     ,ROUND(SUM(log_cost),2) AS log_cost
     ,ROUND(SUM(total_products),0) AS total_products
